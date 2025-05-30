13c43294c33d9cb71361fc86a531de772668786d
Long Dong <longdong32120@gmail.com>
2025-05-30 07:10:19 +0700

    Add files via upload

---
+++ ReuseLibrary/MyUtils.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OOP;
+
+namespace ReuseLibrary
+{
+    public static class MyUtils
+    {
+        public static int countAge(Employee employee)
+        {
+            return DateTime.Now.Year - employee.DOB.Year;
+        }
+    }
+}
---
+++ ReuseLibrary/Program.cs
@@ -0,0 +1,8 @@
+﻿using ExtensionMethod;
+using OOP;
+
+Employee employee = new Employee()
+{
+    Name = "Longie",
+    DOB = new DateTime(2005, 1, 28)
+};
