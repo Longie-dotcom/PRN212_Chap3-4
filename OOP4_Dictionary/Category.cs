6eb8d692e6379e03c85d68224f37c7960703a0c9
Long Dong <longdong32120@gmail.com>
2025-06-02 02:50:18 +0700

    Add files via upload

---
+++ OOP4_Dictionary/Category.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP4_Dictionary
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Dictionary<int, Product> Products { get; set; }
+        public Category() { 
+            Products = new Dictionary<int, Product>();
+        }
+
+        public void AddProduct(Product product)
+        {
+            if (!Products.ContainsKey(product.Id))
+            {
+                Products.Add(product.Id, product);
+            }
+        }
+
+        public void PrintAllProducts()
+        {
+            foreach (KeyValuePair<int, Product> product in Products)
+            {
+                Console.WriteLine(product.Value);
+            }
+        }
+
+        public Product GetProduct(int id)
+        {
+            if (!Products.ContainsKey(id))
+            {
+                return null;
+            }
+            else
+            {
+                return Products[id];
+            }
+        }
+
+        public Dictionary<int, Product> SortProduct()
+        {
+            return Products
+                .OrderBy(item => item.Value.Price).ToDictionary<int, Product>();
+        }
+        public Dictionary<int, Product> ComplexSortProduct()
+        {
+            return Products
+                .OrderByDescending(item => item.Value.Quantity)
+                .OrderBy(item => item.Value.Price)
+                .ToDictionary<int, Product>();
+        }
+
+        public void EditProduct(Product p)
+        {
+            if (!Products.ContainsKey(p.Id))
+            {
+                return;
+            }
+
+            Products[p.Id] = p;
+        }
+
+        public bool RemoveProduct(int id)
+        {
+            if (!Products.ContainsKey(id))
+            {
+                return false;
+            } else
+            {
+                return Products.Remove(id);
+            }
+        }
+    }
+}
---
+++ OOP4_Dictionary/Program.cs
@@ -0,0 +1,65 @@
+﻿using OOP4_Dictionary;
+
+Category category = new Category();
+category.AddProduct(new Product
+{
+    Id = 1,
+    Name = "Test",
+    Price = 122.2f,
+    Quantity = 10
+});
+
+category.AddProduct(new Product
+{
+    Id = 2,
+    Name = "Test3",
+    Price = 444.2f,
+    Quantity = 10
+});
+
+category.AddProduct(new Product
+{
+    Id = 3,
+    Name = "Test3",
+    Price = 444.2f,
+    Quantity = 14
+});
+
+category.AddProduct(new Product
+{
+    Id = 4,
+    Name = "Test2",
+    Price = 3.2f,
+    Quantity = 11
+});
+
+category.AddProduct(new Product
+{
+    Id = 5,
+    Name = "Test3",
+    Price = 444.2f,
+    Quantity = 12
+});
+
+
+Product product = new Product()
+{
+    Id = 1,
+    Name = "Updated",
+    Price = 100000.1f,
+    Quantity = 9999
+};
+
+Console.WriteLine(category.GetProduct(product.Id));
+category.EditProduct(product);
+Console.WriteLine(category.GetProduct(product.Id));
+
+if (category.RemoveProduct(product.Id))
+{
+    Console.WriteLine("Removed successfully!");
+}
+else
+{
+    Console.WriteLine("Can not remove product!");
+};
+
